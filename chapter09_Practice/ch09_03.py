3ff0a9556ef6da724fe99ad815b95918a243074f
ghshli <seohyonili@duksung.ac.kr>
2025-10-06 14:25:24 +0900

    9장 연습문제변경

---
+++ chapter09_Practice/ch09_03.py
@@ -0,0 +1,22 @@
+class Student:
+    def __init__(self,name):
+        self.name=name
+        self.score=[] #빈리스트
+
+    def add_score(self,score):
+        self.score.append(score)
+        print(f"{self.name}의 성적 {score}점이 추가되었습니다.")
+
+    def cal_avg(self):
+        if not self.score:
+            return 0
+        return sum(self.score)/len(self.score) #평균 연산
+    
+stu = Student("Kim")
+
+stu.add_score(90)
+stu.add_score(70)
+stu.add_score(80)
+
+avg = stu.cal_avg()
+print(f"{stu.name}의 평균 성적은 {avg:.2f}")
